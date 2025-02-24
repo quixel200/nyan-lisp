b0737b2f7223c378f152c36572df84683ac1e3c5
quixel200 <blazegaming20057@gmail.com>
2025-02-24 12:27:50 +0530

    added parser to user input

--- parsing.c
+++ parsing.c
@@ -46,7 +46,14 @@ int main(int argc, char **argv){
 
         add_history(input);
 
-        printf("did you just say %s\n",input);
+        mpc_result_t r;
+        if(mpc_parse("<stdin>",input,lispy,&r)){
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        }else{
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
         free(input);
     }
     mpc_cleanup(4,Number,Operator,Expr,lispy);
